9b016f6d1f5744c02a21951433e3596c14aef801
zaid03 <terguyzaid@gmail.com>
2025-10-25 02:48:39 +0100

    -refactoring the front and back to fit facturas only

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { EntComponent } from './ent/ent.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProveedoreesComponent } from './proveedorees/proveedorees.component';
 import { CentrogestorComponent } from './centrogestor/centrogestor.component';
 import { EjeComponent } from './eje/eje.component';
 import { FacturasComponent } from './facturas/facturas.component';
@@ -12,7 +11,6 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'ent', component: EntComponent },
     { path: 'dashboard', component: DashboardComponent },
-    { path: 'proveedorees', component: ProveedoreesComponent },
     { path: 'eje', component: EjeComponent},
     { path: 'centro-gestor', component: CentrogestorComponent},
     { path: 'facturas', component: FacturasComponent},
