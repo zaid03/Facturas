7fd525a467622b5b3ce506f429f7e3f6cb79b271
zaid03 <terguyzaid@gmail.com>
2025-10-25 02:40:09 +0100

    first init

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,27 @@
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { EntComponent } from './ent/ent.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProveedoreesComponent } from './proveedorees/proveedorees.component';
+import { CentrogestorComponent } from './centrogestor/centrogestor.component';
+import { EjeComponent } from './eje/eje.component';
+import { FacturasComponent } from './facturas/facturas.component';
+import { NgModule } from '@angular/core';
+
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'ent', component: EntComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'proveedorees', component: ProveedoreesComponent },
+    { path: 'eje', component: EjeComponent},
+    { path: 'centro-gestor', component: CentrogestorComponent},
+    { path: 'facturas', component: FacturasComponent},
+    { path: '', redirectTo: '/login', pathMatch: 'full' }, //route by default
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+
+export class AppRoutingModule {}
